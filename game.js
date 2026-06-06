d370b62b8b40f0466f5ea25b2be7d54e934f9a40
pingponggemini-pixel <pingponggemini@gmail.com>
2026-06-06 21:09:13 +0800

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,8 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
 
+// 直接在畫布上畫一個橘色正方形（X: 100, Y: 400, 寬: 40, 高: 60）
+ctx.fillStyle = '#ff922b';
+ctx.fillRect(100, 400, 40, 60);
+
+console.log("成功畫出方塊了！");
