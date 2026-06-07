c050fbd3ecb1ef4a79b610faa38203e33d4e3a76
pingponggemini-pixel <pingponggemini@gmail.com>
2026-06-07 09:09:03 +0800

    Update game.js

--- js/game.js
+++ js/game.js
@@ -89,7 +89,7 @@ function fight(name){
 
     while(player.hp > 0 && monster.hp > 0){
 
-        monster.hp -= player.attack;
+        monster.hp -= getAttack();
 
         addLog(
             "你攻擊 " +
